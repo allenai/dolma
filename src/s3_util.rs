279a1cd4e5519b0de490857ae61a3b74ad5a92a4
Luca Soldaini <lucas@allenai.org>
2023-07-03 13:13:31 -0700

    adding utils to split s3 paths

--- src/s3_util.rs
+++ src/s3_util.rs
@@ -1,11 +1,41 @@
 use std::io;
 use std::path::Path;
 
-use aws_sdk_s3::config::Region;
+
+use regex::Regex;
 use aws_sdk_s3::error::ProvideErrorMetadata;
 use aws_sdk_s3::primitives::ByteStream;
 use aws_sdk_s3::Client as S3Client;
 use tokio::fs::File as TokioFile;
+
+
+pub fn split_path(s3_prefix: &str) -> Result<(&str, &str), &'static str> {
+    // use a regular expression to check if s3_prefix starts with s3://
+    let re = Regex::new(r"^s3://").unwrap();
+    if !re.is_match(s3_prefix) {
+        return Err("s3_prefix must start with s3://");
+    }
+
+    // split the s3_prefix into parts
+    let parts: Vec<&str> = s3_prefix.splitn(4, '/').collect();
+
+    // if there are less than 3 parts, then the s3_prefix is invalid
+    if parts.len() < 3 {
+        return Err("s3_prefix must be in the form s3://bucket/path/to/object");
+    }
+
+    let bucket = parts[2];
+
+    // if there are not 4 parts, then the object path is empty, so we set it to "/"
+    let object_path = if parts.len() == 4 {
+        parts[3]
+    } else {
+        "/"
+    };
+
+    Ok((bucket, object_path))
+}
+
 
 pub async fn download_to_file(
     s3_client: &S3Client,
@@ -97,64 +127,73 @@ pub fn find_objects_matching_patterns(
         .unwrap();
 
     let mut stream_inputs: Vec<String> = Vec::new();
-    for pattern in patterns.iter() {
+    for full_pattern in patterns.iter() {
         let start_size = stream_inputs.len();
-        let mut prefix = pattern.clone();
-        let mut suffix: Option<String> = Some("".to_owned());
-        let maybe_index = pattern.chars().position(|c| c == '*');
-        if let Some(index) = maybe_index {
-            prefix = pattern[..index].to_string();
-            suffix = None;
-            if index < pattern.len() - 1 {
-                suffix = Some(pattern[index + 2..].to_string());
+
+        match split_path(full_pattern) {
+            Ok((bucket, pattern)) => {
+                let mut prefix = pattern.clone().to_string();
+                let mut suffix: Option<String> = Some("".to_owned());
+                let maybe_index = pattern.chars().position(|c| c == '*');
+                if let Some(index) = maybe_index {
+                    prefix = pattern[..index].to_string();
+                    suffix = None;
+                    if index < pattern.len() - 1 {
+                        suffix = Some(pattern[index + 2..].to_string());
+                    }
+                }
+                let mut has_more = true;
+                let mut token: Option<String> = None;
+                while has_more {
+                    let resp = if token.is_some() {
+                        rt.block_on(
+                            s3_client
+                                .list_objects_v2()
+                                .bucket(bucket)
+                                .prefix(&prefix)
+                                .delimiter("/")
+                                .continuation_token(token.unwrap())
+                                .send(),
+                        )
+                        .unwrap()
+                    } else {
+                        rt.block_on(
+                            s3_client
+                                .list_objects_v2()
+                                .bucket(bucket)
+                                .prefix(&prefix)
+                                .delimiter("/")
+                                .send(),
+                        )
+                        .unwrap()
+                    };
+                    resp.contents().unwrap_or_default().iter().for_each(|obj| {
+                        stream_inputs.push(obj.key().unwrap().to_owned());
+                    });
+                    suffix.iter().for_each(|s| {
+                        resp.common_prefixes()
+                            .unwrap_or_default()
+                            .iter()
+                            .for_each(|sub_folder| {
+                                let mut full_path = sub_folder.prefix().unwrap().to_owned();
+                                full_path.push_str(s);
+                                stream_inputs.push(full_path);
+                            });
+                    });
+                    token = resp.next_continuation_token().map(String::from);
+                    has_more = token.is_some();
+                }
+                log::info!(
+                    "Found {} objects for pattern \"{}\"",
+                    stream_inputs.len() - start_size,
+                    pattern
+                );
+            }
+            Err(e) => {
+                println!("Error parsing pattern: {}", e);
             }
         }
-        let mut has_more = true;
-        let mut token: Option<String> = None;
-        while has_more {
-            let resp = if token.is_some() {
-                rt.block_on(
-                    s3_client
-                        .list_objects_v2()
-                        .bucket("ai2-llm")
-                        .prefix(&prefix)
-                        .delimiter("/")
-                        .continuation_token(token.unwrap())
-                        .send(),
-                )
-                .unwrap()
-            } else {
-                rt.block_on(
-                    s3_client
-                        .list_objects_v2()
-                        .bucket("ai2-llm")
-                        .prefix(&prefix)
-                        .delimiter("/")
-                        .send(),
-                )
-                .unwrap()
-            };
-            resp.contents().unwrap_or_default().iter().for_each(|obj| {
-                stream_inputs.push(obj.key().unwrap().to_owned());
-            });
-            suffix.iter().for_each(|s| {
-                resp.common_prefixes()
-                    .unwrap_or_default()
-                    .iter()
-                    .for_each(|sub_folder| {
-                        let mut full_path = sub_folder.prefix().unwrap().to_owned();
-                        full_path.push_str(s);
-                        stream_inputs.push(full_path);
-                    });
-            });
-            token = resp.next_continuation_token().map(String::from);
-            has_more = token.is_some();
-        }
-        log::info!(
-            "Found {} objects for pattern \"{}\"",
-            stream_inputs.len() - start_size,
-            pattern
-        );
+
     }
     stream_inputs.sort();
     Ok(stream_inputs)
@@ -166,9 +205,7 @@ pub fn new_client() -> Result<S3Client, io::Error> {
         .build()
         .unwrap();
     let aws_config = rt.block_on(
-        aws_config::from_env()
-            .region(Region::new("us-east-1"))
-            .load(),
+        aws_config::from_env().load(),
     );
     let s3_client = S3Client::new(&aws_config);
     Ok(s3_client)
