234c600758ab0ab7582771122aa7dc3a6626b947
Luca Soldaini <lucas@allenai.org>
2023-07-05 22:19:28 -0700

    imports

--- python/dolma/taggers/__init__.py
+++ python/dolma/taggers/__init__.py
@@ -1,8 +1,1 @@
-from .c4 import *  # noqa: F403
-from .code import *  # noqa: F403
-from .gopher import *  # noqa: F403
-from .jigsaw import *  # noqa: F403
-from .language import *  # noqa: F403
-from .length import *  # noqa: F403
-from .pii import *  # noqa: F403
-from .sampling import *  # noqa: F403
+from . import c4, code, gopher, jigsaw, language, length, pii, sampling
