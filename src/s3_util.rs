d1161489dc5f6976e57c7f1e7d65447d40daa459
chris-ha458 <hac541309@gmail.com>
2023-08-22 20:34:20 +0900

    hacky skip rust tests
    skip s3 related rust tests if
    DOLMA_TESTS_SKIP_AWS=True

--- src/s3_util.rs
+++ src/s3_util.rs
@@ -272,6 +272,10 @@ mod test {
 
     #[test]
     fn test_object_size() -> Result<(), io::Error> {
+        if std::env::var("DOLMA_TESTS_SKIP_AWS").ok().map_or(false, |v| v.to_lowercase() == "true") {
+            println!("Skipping test_download_file because DOLMA_TESTS_SKIP_AWS=True");
+            return Ok(());
+        }
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build()
@@ -288,6 +292,10 @@ mod test {
 
     #[test]
     fn test_download_file() -> Result<(), io::Error> {
+        if std::env::var("DOLMA_TESTS_SKIP_AWS").ok().map_or(false, |v| v.to_lowercase() == "true") {
+            println!("Skipping test_download_file because DOLMA_TESTS_SKIP_AWS=True");
+            return Ok(());
+        }
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build()
@@ -311,6 +319,10 @@ mod test {
 
     #[test]
     fn test_find_objects_matching_patterns() -> Result<(), io::Error> {
+        if std::env::var("DOLMA_TESTS_SKIP_AWS").ok().map_or(false, |v| v.to_lowercase() == "true") {
+            println!("Skipping test_download_file because DOLMA_TESTS_SKIP_AWS=True");
+            return Ok(());
+        }
         let s3_client = new_client(None)?;
 
         let patterns =
