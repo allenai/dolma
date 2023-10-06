9c76d04ff33533763368d5c8f15351268a0be16b
Luca Soldaini <luca@soldaini.net>
2023-10-06 10:04:31 -0700

    fixed tokenizer paths

--- configs/dolma-v1_5/decontamination/tokenize_v3.sh
+++ configs/dolma-v1_5/decontamination/tokenize_v3.sh
@@ -1,72 +1,35 @@
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/c4_en/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/c4_en/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/c4_en/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/c4_en/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
+#! /usr/bin/env bash
 
+datasets=(
+    '4chan_meta_sep'
+    'c4_100_domains'
+    'c4_en'
+    'dolma_100_subreddits'
+    'dolma-v1_5'
+    'falcon-refinedweb'
+    'gab'
+    'ice_fixed'
+    'm2d2_s2orc_unsplit'
+    'm2d2_wikipedia_unsplit'
+    'manosphere_meta_sep'
+    'mc4'
+    'pile'
+    'ptb'
+    'redpajama'
+    'twitterAAE_HELM_fixed'
+    'wikitext_103'
+)
 
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/4chan_meta_sep/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/4chan_meta_sep/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/4chan_meta_sep/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/4chan_meta_sep/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
+splits=(
+    'test'
+    'val'
+)
 
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/c4_100_domains/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/c4_100_domains/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/c4_100_domains/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/c4_100_domains/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/dolma_100_subreddits/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/dolma_100_subreddits/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/dolma_100_subreddits/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/dolma_100_subreddits/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/dolma-v1_5/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/dolma-v1_5/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/dolma-v1_5/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/dolma-v1_5/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/falcon-refinedweb/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/falcon-refinedweb/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/falcon-refinedweb/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/falcon-refinedweb/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/gab/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/gab/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/gab/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/gab/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/ice_fixed/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/ice_fixed/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/ice_fixed/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/ice_fixed/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/m2d2_s2orc_unsplit/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/m2d2_s2orc_unsplit/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/m2d2_s2orc_unsplit/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/m2d2_s2orc_unsplit/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/m2d2_wikipedia_unsplit/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/m2d2_wikipedia_unsplit/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/m2d2_wikipedia_unsplit/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/m2d2_wikipedia_unsplit/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/manosphere_meta_sep/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/manosphere_meta_sep/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/manosphere_meta_sep/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/manosphere_meta_sep/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/mc4/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/mc4/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/mc4/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/mc4/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/pile/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/pile/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/pile/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/pile/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/ptb/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/ptb/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/ptb/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/ptb/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/ptb/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/ptb/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/ptb/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/ptb/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/redpajama/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/redpajama/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/redpajama/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/redpajama/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/twitterAAE_HELM_fixed/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/twitterAAE_HELM_fixed/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/twitterAAE_HELM_fixed/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/twitterAAE_HELM_fixed/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-
-
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/wikitext_103/test' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/wikitext_103/test' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
-dolma tokens --documents 's3://ai2-llm/eval-data/perplexity/v3/wikitext_103/val' --destination 's3://ai2-llm/eval-data/perplexity/v3_gptneox20b/wikitext_103/val' --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
+for dataset in "${datasets[@]}"; do
+    for split in "${splits[@]}"; do
+        dolma tokens \
+            --documents "s3://ai2-llm/eval-data/perplexity/v3_small/${dataset}/${split}" \
+            --destination "s3://ai2-llm/eval-data/perplexity/v3_small_gptneox20b/${dataset}/${split}" \
+            --tokenizer 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
+    done
+done
