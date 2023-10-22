2a1af6e8a1d5184791f7394ccba92a2689acb699
Luca Soldaini <luca@soldaini.net>
2023-10-22 11:41:51 -0700

    run all

--- configs/pes2o-by-years/tokenize.sh
+++ configs/pes2o-by-years/tokenize.sh
@@ -7,7 +7,7 @@ for year in {1970..2023}; do
             --documents "$BASE_S3/split=$split/year=$year/*.gz" \
             --destination "s3://ai2-llm/preprocessed/pes2o/v2-by-year/$split/$year/gpt-neox-20b" \
             --max_size 34359738368 \
-            --tokenizer_name_or_path 'allenai/eleuther-ai-gpt-neox-20b-pii-special'
+            --processes 1 \
+            --tokenizer_name_or_path 'allenai/eleuther-ai-gpt-neox-20b-pii-special' &
     done
-    exit
 done
