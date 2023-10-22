2a990b6f6935f114e3e87d5f94ed95560af62b4a
Luca Soldaini <luca@soldaini.net>
2023-10-22 16:38:47 -0700

    temp

--- configs/pes2o-by-years/tokenize.sh
+++ configs/pes2o-by-years/tokenize.sh
@@ -3,9 +3,9 @@ BASE_S3="s3://ai2-llm/pretraining-data/sources/s2/v3-by-year/documents"
 # for year in {1970..2023}; do
 for year in {2016..2023}; do
     for split in train valid; do
-        # increase number of processes after 2015
-        if [[ $year -gt 2015 ]]; then
-            processes=4
+        # increase number of processes after 2015 and if we're on the train split
+        if [[ $year -gt 2015 ]] && [[ $split == "train" ]]; then
+            processes=2
         else
             processes=1
         fi
@@ -14,7 +14,7 @@ for year in {2016..2023}; do
         dolma tokens \
             --documents "$BASE_S3/split=$split/year=$year/*.gz" \
             --destination "s3://ai2-llm/preprocessed/pes2o/v2-by-year/$split/$year/gpt-neox-20b" \
-            --max_size 4294967296 \
+            --max_size 3294967296 \
             --processes $processes \
             --tokenizer_name_or_path 'allenai/eleuther-ai-gpt-neox-20b-pii-special' &
     done
