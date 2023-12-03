ed7c9900dbc818932ab9c45d66d4f2b4adcc48cf
Luca Soldaini <luca@soldaini.net>
2023-12-03 04:53:50 +0000

    accidentally removed tests

--- tests/python/test_deduper.py
+++ tests/python/test_deduper.py
@@ -21,90 +21,90 @@ DEDUPE_BY_URL = Path(__file__).parent.parent / "config/dedupe-by-url.json"
 DEDUPE_PARAGRAPHS = Path(__file__).parent.parent / "config/dedupe-paragraphs.json"
 
 
-# class TestDeduper(TestCase):
-#     def setUp(self) -> None:
-#         self.stack = ExitStack()
-#         self.local_temp_dir = self.stack.enter_context(TemporaryDirectory()).rstrip("/")
+class TestDeduper(TestCase):
+    def setUp(self) -> None:
+        self.stack = ExitStack()
+        self.local_temp_dir = self.stack.enter_context(TemporaryDirectory()).rstrip("/")
 
-#         if skip_aws_tests():
-#             self.remote_test_prefix = None
-#         else:
-#             self.remote_test_prefix = get_test_prefix()
+        if skip_aws_tests():
+            self.remote_test_prefix = None
+        else:
+            self.remote_test_prefix = get_test_prefix()
 
-#             # upload test data
-#             upload_s3_prefix(
-#                 s3_prefix=f"{self.remote_test_prefix}", local_prefix="tests/data/provided/documents/*.gz"
-#             )
+            # upload test data
+            upload_s3_prefix(
+                s3_prefix=f"{self.remote_test_prefix}", local_prefix="tests/data/provided/documents/*.gz"
+            )
 
-#         # copy provided config files to local temp dir
-#         shutil.copytree(
-#             "tests/data/provided/documents",
-#             f"{self.local_temp_dir}/tests/data/provided/documents",
-#             dirs_exist_ok=True,
-#         )
+        # copy provided config files to local temp dir
+        shutil.copytree(
+            "tests/data/provided/documents",
+            f"{self.local_temp_dir}/tests/data/provided/documents",
+            dirs_exist_ok=True,
+        )
 
-#     def tearDown(self) -> None:
-#         if self.remote_test_prefix is not None:
-#             clean_test_data(self.remote_test_prefix)
-#         self.stack.close()
+    def tearDown(self) -> None:
+        if self.remote_test_prefix is not None:
+            clean_test_data(self.remote_test_prefix)
+        self.stack.close()
 
-#     def test_dedupe_by_url(self):
-#         with open(DEDUPE_BY_URL, "r") as f:
-#             config = json.load(f)
+    def test_dedupe_by_url(self):
+        with open(DEDUPE_BY_URL, "r") as f:
+            config = json.load(f)
 
-#         config["documents"][0] = f'{self.local_temp_dir}/{config["documents"][0]}'
-#         config["bloom_filter"]["file"] = f'{self.local_temp_dir}/{config["bloom_filter"]["file"]}'
+        config["documents"][0] = f'{self.local_temp_dir}/{config["documents"][0]}'
+        config["bloom_filter"]["file"] = f'{self.local_temp_dir}/{config["bloom_filter"]["file"]}'
 
-#         with NamedTemporaryFile("w") as f:
-#             json.dump(config, f)
-#             f.flush()
+        with NamedTemporaryFile("w") as f:
+            json.dump(config, f)
+            f.flush()
 
-#             main(argv=["-c", f.name, "dedupe"])
+            main(argv=["-c", f.name, "dedupe"])
 
-#         expected = load_jsonl("tests/data/expected/dedupe-by-url.json.gz")
-#         computed = load_jsonl(f"{self.local_temp_dir}/tests/data/provided/attributes/dedupe_by_url/000.json.gz")
-#         self.assertEqual(expected, computed)
+        expected = load_jsonl("tests/data/expected/dedupe-by-url.json.gz")
+        computed = load_jsonl(f"{self.local_temp_dir}/tests/data/provided/attributes/dedupe_by_url/000.json.gz")
+        self.assertEqual(expected, computed)
 
-#     def test_dedupe_paragraphs(self):
-#         with open(DEDUPE_PARAGRAPHS, "r") as f:
-#             config = json.load(f)
+    def test_dedupe_paragraphs(self):
+        with open(DEDUPE_PARAGRAPHS, "r") as f:
+            config = json.load(f)
 
-#         config["documents"][0] = f'{self.local_temp_dir}/{config["documents"][0]}'
-#         config["bloom_filter"]["file"] = f'{self.local_temp_dir}/{config["bloom_filter"]["file"]}'
+        config["documents"][0] = f'{self.local_temp_dir}/{config["documents"][0]}'
+        config["bloom_filter"]["file"] = f'{self.local_temp_dir}/{config["bloom_filter"]["file"]}'
 
-#         with NamedTemporaryFile("w") as f:
-#             json.dump(config, f)
-#             f.flush()
+        with NamedTemporaryFile("w") as f:
+            json.dump(config, f)
+            f.flush()
 
-#             main(argv=["-c", f.name, "dedupe"])
+            main(argv=["-c", f.name, "dedupe"])
 
-#         expected = load_jsonl("tests/data/expected/dedupe-paragraphs.json.gz")
-#         computed = load_jsonl(
-#             f"{self.local_temp_dir}/tests/data/provided/attributes/dedupe_paragraphs/000.json.gz"
-#         )
-#         self.assertEqual(expected, computed)
+        expected = load_jsonl("tests/data/expected/dedupe-paragraphs.json.gz")
+        computed = load_jsonl(
+            f"{self.local_temp_dir}/tests/data/provided/attributes/dedupe_paragraphs/000.json.gz"
+        )
+        self.assertEqual(expected, computed)
 
-#     def test_dedupe_by_url_remote_input(self):
-#         if self.remote_test_prefix is None:
-#             return self.skipTest("Skipping AWS tests")
+    def test_dedupe_by_url_remote_input(self):
+        if self.remote_test_prefix is None:
+            return self.skipTest("Skipping AWS tests")
 
-#         with open(DEDUPE_BY_URL, "r") as f:
-#             config = json.load(f)
+        with open(DEDUPE_BY_URL, "r") as f:
+            config = json.load(f)
 
-#         config["documents"][0] = f'{self.remote_test_prefix}/{config["documents"][0]}'
-#         config["bloom_filter"]["file"] = f'{self.local_temp_dir}/{config["bloom_filter"]["file"]}'
+        config["documents"][0] = f'{self.remote_test_prefix}/{config["documents"][0]}'
+        config["bloom_filter"]["file"] = f'{self.local_temp_dir}/{config["bloom_filter"]["file"]}'
 
-#         with NamedTemporaryFile("w") as f:
-#             json.dump(config, f)
-#             f.flush()
+        with NamedTemporaryFile("w") as f:
+            json.dump(config, f)
+            f.flush()
 
-#             main(argv=["-c", f.name, "dedupe"])
+            main(argv=["-c", f.name, "dedupe"])
 
-#         download_s3_prefix(self.remote_test_prefix, self.local_temp_dir)
+        download_s3_prefix(self.remote_test_prefix, self.local_temp_dir)
 
-#         expected = load_jsonl("tests/data/expected/dedupe-by-url.json.gz")
-#         computed = load_jsonl(f"{self.local_temp_dir}/tests/data/provided/attributes/dedupe_by_url/000.json.gz")
-#         self.assertEqual(expected, computed)
+        expected = load_jsonl("tests/data/expected/dedupe-by-url.json.gz")
+        computed = load_jsonl(f"{self.local_temp_dir}/tests/data/provided/attributes/dedupe_by_url/000.json.gz")
+        self.assertEqual(expected, computed)
 
 
 class TestDeduperPipeline(TestCasePipeline):
@@ -171,5 +171,3 @@ class TestDeduperPipeline(TestCasePipeline):
         self.assertEqual(v1, 1.0)
         self.assertEqual(documents[1][s2:e2], duplicate_text)
         self.assertEqual(v2, 1.0)
-
-    # def test_min_length
