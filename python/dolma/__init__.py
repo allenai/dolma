71b3858dc9d85833235faef512cf150ff28aae64
Luca Soldaini <luca@soldaini.net>
2024-02-05 19:03:16 -0800

    url testing

--- python/dolma/__init__.py
+++ python/dolma/__init__.py
@@ -1,5 +1,10 @@
 import json
+from pathlib import Path
 import warnings
+import urllib3.util
+from typing import List, Optional, Union
+
+import smart_open
 
 # warning raised by pkg_resources used in a lot of google packages
 warnings.filterwarnings("ignore", message=r".*declare_namespace\(\'.*google.*", category=DeprecationWarning)
@@ -24,6 +29,16 @@ add_tagger = TaggerRegistry.add
 
 
 def deduper(config: dict):
+    """
+    Run the deduper with the given configuration.
+
+    Args:
+        config (dict): The configuration for the deduper.
+
+    Raises:
+        DolmaRustPipelineError: If there is an error running the deduper.
+
+    """
     try:
         _dolma.deduper_entrypoint(json.dumps(config))
     except RuntimeError as e:
@@ -31,7 +46,103 @@ def deduper(config: dict):
 
 
 def mixer(config: dict):
+    """
+    Run the mixer with the given configuration.
+
+    Args:
+        config (dict): A dictionary containing the configuration parameters for the mixer.
+
+    Raises:
+        DolmaRustPipelineError: If an error occurs while running the mixer.
+    """
     try:
         _dolma.mixer_entrypoint(json.dumps(config))
     except RuntimeError as e:
         raise DolmaRustPipelineError(f"Error running mixer: {e}") from e
+
+
+class UrlBlocker:
+    """
+    A class that provides URL blocking functionality based on a set of rules.
+
+    Args:
+        rules (List[str]): A list of rules to be used for blocking URLs.
+
+    Attributes:
+        engine: The underlying engine used for URL blocking.
+
+    Methods:
+        from_adblockplus_filepath: Create an instance of UrlBlocker from an AdBlock Plus file.
+        check_network_urls: Check if a given URL should be blocked based on the rules.
+
+    """
+
+    def __init__(
+        self,
+        rules: List[str],
+    ) -> None:
+        """
+        Initialize the UrlBlocker instance.
+
+        Args:
+            rules (List[str]): A list of rules to be used for blocking URLs.
+
+        """
+        self.engine = _dolma.UrlBlocker(rules=rules)
+
+    @classmethod
+    def from_adblockplus_filepath(
+        cls,
+        adblockplus_filepath: Union[str, Path],
+    ) -> "UrlBlocker":
+        """
+        Create an instance of UrlBlocker from an AdBlock Plus file.
+
+        Args:
+            adblockplus_filepath (str): The filepath of the AdBlock Plus file.
+
+        Returns:
+            UrlBlocker: An instance of UrlBlocker created from the AdBlock Plus file.
+
+        """
+        with smart_open.open(adblockplus_filepath, 'rt') as adb_file:
+            rules = [
+                ln.strip()
+                for ln in adb_file
+                if not ln.startswith('!')
+            ]
+        return cls(rules)
+
+    def check_network_urls(
+        self,
+        url: str,
+        source_url: Optional[str] = None,
+        request_type: str = "",
+    ) -> bool:
+        """
+        Check if a given URL should be blocked based on the rules.
+
+        Args:
+            url (str): The URL to be checked.
+            source_url (str): The source URL of the request. If not provided, the host from the URL will be used.
+            request_type (str): The type of the request. For a list of valid request types, see the adblockplus
+                documentation: https://help.adblockplus.org/hc/en-us/articles/360062733293-How-to-write-filters
+
+        Returns:
+            bool: True if the URL should be blocked, False otherwise.
+
+        """
+        parsed = urllib3.util.parse_url(url)
+        if parsed.scheme is None:
+            # if the URL does not have a scheme, we assume it is an HTTP URL
+            url = f"http://{url}"
+
+        if source_url is None:
+            # if the source URL is not provided, we use the host from the URL
+            source_url = parsed.host
+
+        return self.engine.check_network_urls(
+            url=url,
+            source_url=source_url,
+            request_type=request_type,
+        )
---
+++ tests/python/test_urls.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from unittest import TestCase
+from dolma import UrlBlocker
+
+LOCAL_DATA = Path(__file__).parent.parent / "data"
+
+
+class TestUrlBlocker(TestCase):
+    def test_brave_adblocker(self):
+        rules = [
+            "-advertisement-icon.",
+            "-advertisement-management/",
+            "-advertisement.",
+            "-advertisement/script.",
+        ]
+        engine = UrlBlocker(rules)
+
+        to_block = "http://example.com/-advertisement-icon."
+        base_url = "http://example.com/helloworld"
+        self.assertTrue(engine.check_network_urls(to_block, base_url, "image"))
+        self.assertTrue(engine.check_network_urls(to_block, base_url))
+        self.assertTrue(engine.check_network_urls(to_block))
+        self.assertFalse(engine.check_network_urls(to_block, None, "document"))
+
+        not_to_block = "http://example.com/main-icon"
+        self.assertFalse(engine.check_network_urls(not_to_block, base_url, "image"))
+        self.assertFalse(engine.check_network_urls(not_to_block, base_url))
+        self.assertFalse(engine.check_network_urls(not_to_block))
+
+    def test_load_from_file(self):
+        engine = UrlBlocker.from_adblockplus_filepath(LOCAL_DATA / "urls/easylist.txt.gz")
+
+        # global rules
+        self.assertTrue(engine.check_network_urls("berush.com"))
+        self.assertFalse(engine.check_network_urls("example.com"))
+
+        # image rules
+        self.assertTrue(engine.check_network_urls("pjatr.com", None, "image"))
+        self.assertFalse(engine.check_network_urls("pjatr.com", None, "document"))
