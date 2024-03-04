a9774a2056552a145e3f3edfa43eae932a82bf51
Luca Soldaini <lucas@allenai.org>
2024-03-04 06:35:53 -0800

    c4 tokenize

---
+++ configs/dolma-v1_7/sources/c4.sh
@@ -0,0 +1,33 @@
+set -ex
+
+NUMBER_OF_CORES=188
+
+dolma tokens \
+    --documents 's3://ai2-llm/pretraining-data/sources/c4/v1_dd_ngram_doc_le030/*.gz' \
+    --destination 's3://ai2-llm/preprocessed/c4/v1_dd_ngram_doc_le030/gpt-neox-olmo-dolma-v1_5' \
+    --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
+    --max_size '4_294_967_296' \
+    --seed 0 \
+    --tokenizer.eos_token_id 50279 \
+    --tokenizer.pad_token_id 1 \
+    --processes "${NUMBER_OF_CORES}"
+
+dolma tokens \
+    --documents 's3://ai2-llm/pretraining-data/sources/c4/v1_dd_ngram_docpara_le030/*.gz' \
+    --destination 's3://ai2-llm/preprocessed/c4/v1_dd_ngram_docpara_le030/gpt-neox-olmo-dolma-v1_5' \
+    --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
+    --max_size '4_294_967_296' \
+    --seed 0 \
+    --tokenizer.eos_token_id 50279 \
+    --tokenizer.pad_token_id 1 \
+    --processes "${NUMBER_OF_CORES}"
+
+# dolma tokens \
+#     --documents 's3://ai2-llm/pretraining-data/sources/olmo-mix/v1_6-decontaminated/documents/cc_en_tail/*.gz' \
+#     --destination 's3://ai2-llm/preprocessed/olmo-mix/v1_6-decontaminated/cc_en_tail/gpt-neox-olmo-dolma-v1_5' \
+#     --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
+#     --max_size '20_147_483_648' \
+#     --seed 0 \
+#     --tokenizer.eos_token_id 50279 \
+#     --tokenizer.pad_token_id 1 \
+#     --processes "${NUMBER_OF_CORES}"
