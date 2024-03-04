86800847b44b79303a1f781b6a6114b34753e4e8
Luca Soldaini <lucas@allenai.org>
2024-03-04 06:58:41 -0800

    cc

--- configs/dolma-v1_7/sources/olmo_mix.sh
+++ configs/dolma-v1_7/sources/olmo_mix.sh
@@ -1,10 +1,20 @@
 set -ex
 
-NUMBER_OF_CORES=188
+
+export DOLMA_SUBSET=${1}
+
+# check if the subset is provided
+if [ -z "${DOLMA_SUBSET}" ]; then
+    echo "Please provide the subset to process"
+    exit 1
+fi
+
+# reseerve 4 cores for the system
+export NUMBER_OF_CORES=$(($(nproc) - 4))
 
 dolma tokens \
-    --documents 's3://ai2-llm/pretraining-data/sources/olmo-mix/v1_6-decontaminated/documents/cc_en_head/*.gz' \
-    --destination 's3://ai2-llm/preprocessed/olmo-mix/v1_6-decontaminated/cc_en_head/gpt-neox-olmo-dolma-v1_5' \
+    --documents 's3://ai2-llm/pretraining-data/sources/olmo-mix/${oc.env:DOLMA_SUBSET}/documents/cc_en_head/*.gz' \
+    --destination '${oc.env:HOME}/ai2-llm/preprocessed/olmo-mix/${oc.env:DOLMA_SUBSET}/cc_en_head/gpt-neox-olmo-dolma-v1_5' \
     --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
     --max_size '20_147_483_648' \
     --seed 0 \
@@ -13,8 +23,8 @@ dolma tokens \
     --processes "${NUMBER_OF_CORES}"
 
 dolma tokens \
-    --documents 's3://ai2-llm/pretraining-data/sources/olmo-mix/v1_6-decontaminated/documents/cc_en_middle/*.gz' \
-    --destination 's3://ai2-llm/preprocessed/olmo-mix/v1_6-decontaminated/cc_en_middle/gpt-neox-olmo-dolma-v1_5' \
+    --documents 's3://ai2-llm/pretraining-data/sources/olmo-mix/${oc.env:DOLMA_SUBSET}/documents/cc_en_middle/*.gz' \
+    --destination '${oc.env:HOME}/ai2-llm/preprocessed/olmo-mix/${oc.env:DOLMA_SUBSET}/cc_en_middle/gpt-neox-olmo-dolma-v1_5' \
     --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
     --max_size '20_147_483_648' \
     --seed 0 \
@@ -22,12 +32,13 @@ dolma tokens \
     --tokenizer.pad_token_id 1 \
     --processes "${NUMBER_OF_CORES}"
 
-# dolma tokens \
-#     --documents 's3://ai2-llm/pretraining-data/sources/olmo-mix/v1_6-decontaminated/documents/cc_en_tail/*.gz' \
-#     --destination 's3://ai2-llm/preprocessed/olmo-mix/v1_6-decontaminated/cc_en_tail/gpt-neox-olmo-dolma-v1_5' \
-#     --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
-#     --max_size '20_147_483_648' \
-#     --seed 0 \
-#     --tokenizer.eos_token_id 50279 \
-#     --tokenizer.pad_token_id 1 \
-#     --processes "${NUMBER_OF_CORES}"
+
+dolma tokens \
+    --documents 's3://ai2-llm/pretraining-data/sources/olmo-mix/${oc.env:DOLMA_SUBSET}/documents/cc_en_tail/*.gz' \
+    --destination '${oc.env:HOME}/ai2-llm/preprocessed/olmo-mix/${oc.env:DOLMA_SUBSET}/cc_en_tail/gpt-neox-olmo-dolma-v1_5' \
+    --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
+    --max_size '20_147_483_648' \
+    --seed 0 \
+    --tokenizer.eos_token_id 50279 \
+    --tokenizer.pad_token_id 1 \
+    --processes "${NUMBER_OF_CORES}"
