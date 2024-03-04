d948590c7a7bc2f645b5bf249d32df86e1da515e
Luca Soldaini <lucas@allenai.org>
2024-03-04 14:47:36 -0800

    local

--- configs/dolma-v1_7/sources/c4.sh
+++ configs/dolma-v1_7/sources/c4.sh
@@ -3,8 +3,8 @@ set -ex
 NUMBER_OF_CORES=188
 
 dolma tokens \
-    --documents 's3://ai2-llm/pretraining-data/sources/c4/v1_dd_ngram_doc_le030/documents/*.gz' \
-    --destination 's3://ai2-llm/preprocessed/c4/v1_dd_ngram_doc_le030/gpt-neox-olmo-dolma-v1_5' \
+    --documents '${oc.env:HOME}/ai2-llm/pretraining-data/sources/c4/v1_dd_ngram_doc_le030/documents/*.gz' \
+    --destination '${oc.env:HOME}/ai2-llm/preprocessed/c4/v1_dd_ngram_doc_le030/gpt-neox-olmo-dolma-v1_5' \
     --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
     --max_size '4_294_967_296' \
     --seed 0 \
@@ -13,8 +13,8 @@ dolma tokens \
     --processes "${NUMBER_OF_CORES}"
 
 dolma tokens \
-    --documents 's3://ai2-llm/pretraining-data/sources/c4/v1_dd_ngram_docpara_le030/documents/*.gz' \
-    --destination 's3://ai2-llm/preprocessed/c4/v1_dd_ngram_docpara_le030/gpt-neox-olmo-dolma-v1_5' \
+    --documents '${oc.env:HOME}/ai2-llm/pretraining-data/sources/c4/v1_dd_ngram_docpara_le030/documents/*.gz' \
+    --destination '${oc.env:HOME}/ai2-llm/preprocessed/c4/v1_dd_ngram_docpara_le030/gpt-neox-olmo-dolma-v1_5' \
     --tokenizer.name_or_path 'allenai/gpt-neox-olmo-dolma-v1_5' \
     --max_size '4_294_967_296' \
     --seed 0 \
