5107c346579c67f427a9424819c35c01f098b497
Luca Soldaini <lucas@allenai.org>
2024-06-09 23:48:02 +0000

    discarding fields

--- configs/cccc/run-dedupe-para-step-1.sh
+++ configs/cccc/run-dedupe-para-step-1.sh
@@ -2,18 +2,32 @@
 
 set -ex
 
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2014-42.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2014-49.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2016-07.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2016-18.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2016-22.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2017-09.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2017-22.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2017-26.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2018-13.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2018-22.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2018-26.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2018-30.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2019-04.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2019-09.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2019-13.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2019-35.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2019-39.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2020-29.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2020-34.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2021-17.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2021-43.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2021-49.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2022-05.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2023-06.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2023-14.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2023-23.yaml dedupe
+dolma -c configs/cccc/dedupe-para/CC-MAIN-2023-50.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2024-10.yaml dedupe
 dolma -c configs/cccc/dedupe-para/CC-MAIN-2024-18.yaml dedupe
