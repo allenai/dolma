9a86e094bcc51ab072f2ba388bfca12de4bcea6f
Luca Soldaini <lucas@allenai.org>
2024-06-09 05:46:15 +0000

    dedup

---
+++ configs/cccc/run-dedupe-doc-step-2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2013-20.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2013-48.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2014-10.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2014-15.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2014-23.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2014-35.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2014-41.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2014-52.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2015-06.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2015-11.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2015-14.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2015-18.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2015-22.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2015-27.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2016-26.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2016-30.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2017-04.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2017-13.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2017-17.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2018-09.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2018-47.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2018-51.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2019-30.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2020-24.yaml dedupe
+dolma -c configs/cccc/dedupe-doc/CC-MAIN-2021-39.yaml dedupe
