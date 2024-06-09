ed9cf902eb0a40abd421b2741996bf66c817f6c8
Luca Soldaini <lucas@allenai.org>
2024-06-09 01:45:21 +0000

    para

--- configs/cccc/run-mix-part-1.sh
+++ configs/cccc/run-mix-part-1.sh
@@ -2,20 +2,20 @@
 
 set -ex
 
-dolma -c configs/cccc/mixer/CC-MAIN-2019-35.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2019-09.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2022-05.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2021-17.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2021-49.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2024-18.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2023-14.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2020-29.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2020-34.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2016-22.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2024-10.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2018-30.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2019-13.yaml mix
-dolma -c configs/cccc/mixer/CC-MAIN-2017-26.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2019-35.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2019-09.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2022-05.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2021-17.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2021-49.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2024-18.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2023-14.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2020-29.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2020-34.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2016-22.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2024-10.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2018-30.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2019-13.yaml mix
+# dolma -c configs/cccc/mixer/CC-MAIN-2017-26.yaml mix
 dolma -c configs/cccc/mixer/CC-MAIN-2018-26.yaml mix
 dolma -c configs/cccc/mixer/CC-MAIN-2021-43.yaml mix
 dolma -c configs/cccc/mixer/CC-MAIN-2017-51.yaml mix
