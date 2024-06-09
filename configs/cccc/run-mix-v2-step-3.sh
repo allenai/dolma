6102ef809c1cb3c831ef337454b05d0ee5304d39
Luca Soldaini <lucas@allenai.org>
2024-06-09 21:20:47 +0000

    keyword

--- configs/cccc/run-mix-v2-step-3.sh
+++ configs/cccc/run-mix-v2-step-3.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-dolma -c configs/cccc/mix-v2/CC-MAIN-2016-26.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2016-30.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2017-30.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2018-09.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2019-43.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2019-51.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2020-10.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2020-24.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2020-40.yaml dedupe
-dolma -c configs/cccc/mix-v2/CC-MAIN-2023-40.yaml dedupe
+dolma -c configs/cccc/mix-v2/CC-MAIN-2016-26.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2016-30.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2017-30.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2018-09.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2019-43.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2019-51.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2020-10.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2020-24.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2020-40.yaml mix
+dolma -c configs/cccc/mix-v2/CC-MAIN-2023-40.yaml mix
