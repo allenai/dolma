41e28f2003fd682e63cca8b04edc3f1301c164f1
Luca Soldaini <lucas@allenai.org>
2024-08-29 16:08:46 -0700

    multinode, wandb

--- scripts/fineweb_classifier.py
+++ scripts/fineweb_classifier.py
@@ -18,7 +18,7 @@ import os
 import time
 from itertools import zip_longest
 
-from typing import TYPE_CHECKING, List, Dict, Any, Optional
+from typing import TYPE_CHECKING, List, Optional, Tuple
 
 import smart_open
 import necessary
@@ -34,7 +34,6 @@ with necessary.necessary("tqdm") as TQDM_AVAILABLE:
 with necessary.necessary("torch") as TORCH_AVAILABLE:
     if TYPE_CHECKING or TORCH_AVAILABLE:
         import torch  # pyright: ignore
-        from torch.utils.data import Dataset  # pyright: ignore
         import torch.distributed as dist # pyright: ignore
 
 with necessary.necessary("transformers") as TRANSFORMERS_AVAILABLE:
@@ -46,38 +45,60 @@ with necessary.necessary("transformers") as TRANSFORMERS_AVAILABLE:
             AutoTokenizer
         )
 
-
-class JsonlDataset(Dataset):
-    """Custom Dataset for JSONL files."""
-    def __init__(self, file_path: str) -> None:
-        self.lines: List[Dict[str, Any]] = []
-        with smart_open.open(file_path, 'rt') as f:
-            for line in f:
-                self.lines.append(json.loads(line))
-
-    def __len__(self) -> int:
-        return len(self.lines)
-
-    def __getitem__(self, idx: int) -> Dict[str, Any]:
-        return self.lines[idx]
+with necessary.necessary("wandb") as WANDB_AVAILABLE:
+    if TYPE_CHECKING or WANDB_AVAILABLE:
+        import wandb
 
 
-def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+def get_rank_and_world_size():
+    if dist.is_initialized():
+        return dist.get_rank(), dist.get_world_size()
+    else:
+        return 0, 1
+
+
+def setup() -> Tuple[int, int]:
+    if (rank := os.environ.get("RANK")) and (world_size := os.environ.get("WORLD_SIZE")):
+        dist.init_process_group("nccl", rank=int(rank), world_size=int(world_size))
+    return get_rank_and_world_size()
 
 def cleanup():
     dist.destroy_process_group()
+
 
 def load_model(model_name: str, rank: int) -> PreTrainedModel:
     """Loads the model onto the specified GPU."""
     device = torch.device(f'cuda:{rank}')
     model = AutoModelForSequenceClassification.from_pretrained(model_name, torch_dtype=torch.bfloat16)
     model.to(device)
-    model = torch.compile(model)
-    return model
+    model = torch.compile(model) # pyright: ignore
+    return model    # pyright: ignore
 
+
+class WandbLogger:
+    is_initialized = False
+    use_wandb = False
+    project = "fineweb-classifier"
+    entity = "ai2"
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.is_initialized and cls.use_wandb:
+            wandb.init(project=cls.project, entity=cls.entity)
+            cls.is_initialized = True
+        return super().__new__(cls, *args, **kwargs)
+
+    def __init__(self):
+        self.rank, self.world_size = get_rank_and_world_size()
+
+    def log(self, **kwargs):
+        if self.use_wandb and self.rank == 0:
+            if (step := kwargs.pop('step', None)):
+                wandb.log(kwargs, step=step)
+            else:
+                wandb.log(kwargs)
+
+        for k, v in kwargs.items():
+            print(f"{self.rank}/{self.world_size} {k}: {v}")
 
 
 def make_prediction(
@@ -117,8 +138,6 @@ def process_file(
     model: PreTrainedModel,
     tokenizer: PreTrainedTokenizer,
     batch_size: int,
-    rank: int,
-    world_size: int,
     max_length: Optional[int] = None,
 ):
     """Processes a single JSONL file and classifies the text."""
@@ -126,6 +145,8 @@ def process_file(
     model.eval()
     model_name = model.config.name_or_path.replace('/', '_')
     start_time = time.time()
+
+    logger = WandbLogger()
 
     with torch.no_grad():
         with smart_open.open(source_path, 'rt') as source_file, \
@@ -135,7 +156,7 @@ def process_file(
             for i, line in enumerate(source_file):
                 if i % 10_000 == 0:
                     throughput = i / (time.time() - start_time)
-                    print(f"{i:,} docs on GPU {rank}/{world_size} from {source_path} ({throughput:.2f} docs/s)")
+                    logger.log(step=i, throughput=throughput)
 
                 batch.append(json.loads(line))
 
@@ -157,6 +178,27 @@ def process_file(
                     destination_file.write(json.dumps(attribute) + '\n')
 
 
+def async_sync_counts(counts: int) -> int:
+    """
+    Asynchronously reduces the counts across all GPUs and returns the result.
+    """
+    # Create an asynchronous CUDA stream
+    stream = torch.cuda.Stream()
+
+    # Move counts to the GPU
+    tensor_counts = torch.tensor(counts).cuda()
+
+    # Perform all_reduce operation asynchronously
+    with torch.cuda.stream(stream): # pyright: ignore
+        dist.all_reduce(tensor_counts, op=dist.ReduceOp.SUM)
+
+    # Stream synchronization ensures that the counts are reduced
+    # without blocking other operations
+    stream.synchronize()
+
+    return int(tensor_counts.item())
+
+
 def process_documents(
     rank: int,
     world_size: int,
@@ -167,13 +209,12 @@ def process_documents(
     max_length: Optional[int] = None
 ):
     """Processes a batch of files using distributed processing."""
-    setup(rank, world_size)
+    setup()
     model = load_model(model_name, rank)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     s3 = s3fs.S3FileSystem()
 
     for source_path, destination_path in zip(source_paths, destination_paths):
-
         if s3.exists(destination_path):
             print(f"Skipping {source_path} on GPU {rank}/{world_size} because {destination_path} already exists")
             continue
@@ -183,8 +224,6 @@ def process_documents(
             destination_path=destination_path,
             model=model,
             tokenizer=tokenizer,
-            rank=rank,
-            world_size=world_size,
             batch_size=batch_size,
             max_length=max_length,
         )
@@ -210,11 +249,12 @@ def longest_common_sequence(strings):
 
 
 def main(args: argparse.Namespace) -> None:
+    rank, world_size = setup()
+    WandbLogger()   # Initialize WandbLogger if use_wandb is True
+
     if not torch.cuda.is_available():
         raise RuntimeError("No GPUs available, but the script is designed to use multiple GPUs.")
 
-    world_size = int(os.environ.get("WORLD_SIZE", 1))
-    rank = int(os.environ.get("RANK", 0))
 
     s3 = s3fs.S3FileSystem()
     source_paths = [f's3://{p}' for p in s3.glob(args.source_prefix)]
@@ -252,6 +292,9 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--batch-size', type=int, default=32, help='Batch size for processing (default: 32)')
     parser.add_argument('--model-name', type=str, default="HuggingFaceFW/fineweb-edu-classifier", help='Hugging Face model name (default: allenai/fineweb-edu-classifier)')
     parser.add_argument('--max-length', type=int, default=None, help='Maximum sequence length for tokenization (default: None)')
+    parser.add_argument('--use-wandb', action='store_true', help='Use Weights & Biases for logging')
+    parser.add_argument('--wandb-project', type=str, default="fineweb-classifier", help='Weights & Biases project name')
+    parser.add_argument('--wandb-entity', type=str, default="ai2", help='Weights & Biases entity name')
     opts = parser.parse_args()
 
     if opts.output_prefix is None:
@@ -259,6 +302,12 @@ def parse_args() -> argparse.Namespace:
             raise ValueError("Output prefix is required unless source prefix contains 'documents'")
         base, _ = opts.source_prefix.split("/documents/", 1)
         opts.output_prefix = f"{base}/attributes/fineweb-edu-classifier"
+
+    if opts.use_wandb:
+        WandbLogger.use_wandb = True
+        WandbLogger.project = opts.wandb_project
+        WandbLogger.entity = opts.wandb_entity
+
     return opts
 
 
