50418653fb687012895bc088dab8a6199269af43
Allyson Ettinger <allysone@mosaic-cirrascale-04.reviz.ai2.in>
2024-09-05 14:46:26 +0000

    clean up comments

--- python/dolma/taggers/reddit.py
+++ python/dolma/taggers/reddit.py
@@ -13,40 +13,6 @@ from ..core.taggers import BaseTagger,BaseTaggerWithMetadata
 from ..core.utils import split_paragraphs
 
 
-
-"""
-CANDIDATE TAGGERS
-
-METADATA (hopefully)
-num_comment
-Score
-Edited/deleted/hidden/pinned (submissions)
-NSFW
-Contains media
-Contains URL
-Edited, stickied, collapsed, self-comment, removed (comments)
-over_18
-
-EXISTING TAGGERS
-Length in chars
-
-NEW TAGGERS
-Amount of length that is not URL
-Length in BPE tokens
-Len longest string without spaces
-Whether starts with non-ascii char
-
-NEW TAGGERS NEEDING LISTS
-Known bot
-Known non-English subreddit
-Member of set of subreddits
-
-NOT SURE YET
-Depth in thread
-
-
-"""
-
 @TaggerRegistry.add("contains_url")
 class ContainsURL(BaseTagger):
 
@@ -60,7 +26,6 @@ class LenWithoutURL(BaseTagger):
 
     def predict(self, doc: Document) -> DocResult:
         url_regex = re.compile("https?://[^\s]+")
-        # url_regex = re.compile("https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)")
         score = len(re.sub(url_regex,"",doc.text))
         return DocResult(doc=doc, spans=[Span(start=0, end=len(doc.text), type="length", score=score)])
 
@@ -78,7 +43,6 @@ class BPETokenizer(BaseTagger):
 
 @TaggerRegistry.add("longest_string")
 class LongestString(BaseTagger):
-    # WHITESPACE_REGEX = regex.compile(r"\w+|[^\w\s]+")
 
     def predict(self, doc: Document) -> DocResult:
         score = max([len(x) for x in doc.text.split()])
@@ -105,8 +69,6 @@ class BannedSubs(ListMembership):
 
     def predict(self, doc: Document) -> DocResult:
         score = doc.metadata["subreddit"].lower() in self.blocklist
-        if score:
-            print(doc)
         return DocResult(doc=doc, spans=[Span(start=0, end=len(doc.text), type="doc", score=score)])
 
 @TaggerRegistry.add("non_english_subreddit")
