42be092597ae9d57c3ff95e2599efa521c7db6d4
Luca Soldaini <lucas@allenai.org>
2024-10-23 22:41:55 -0700

    deberta quality

--- classifiers/src/dolma_classifiers/models.py
+++ classifiers/src/dolma_classifiers/models.py
@@ -2,13 +2,18 @@ from functools import partial
 from typing import NamedTuple, Type
 
 import torch
+from huggingface_hub import PyTorchModelHubMixin
+from torch import nn
 from torch.nn import functional as F
 from transformers import (
+    AutoConfig,
+    AutoModel,
     AutoModelForSequenceClassification,
     AutoTokenizer,
     PreTrainedModel,
     PreTrainedTokenizer,
 )
+from transformers.modeling_outputs import SequenceClassifierOutput
 
 from .loggers import get_logger
 from .utils import get_local_gpu_rank, sanitize_model_name
@@ -23,19 +28,22 @@ class BaseQualityClassifier:
     model: PreTrainedModel
     tokenizer: PreTrainedTokenizer
 
-    def __init__(self, model_name: str, device: str, dtype: str, compile: bool = False):
-        self.model = AutoModelForSequenceClassification.from_pretrained(
-            pretrained_model_name_or_path=model_name,
-            torch_dtype=getattr(torch, dtype),
-            trust_remote_code=True,
-        ).to(torch.device(device))
-
-        if compile:
-            self.model = torch.compile(self.model)  # pyright: ignore
-
+    def __init__(
+        self,
+        model_name: str,
+        device: str,
+        dtype: str,
+        compile: bool = False,
+        trust_remote_code: bool = False,
+    ):
+        self.model = self._make_model(
+            model_name=model_name,
+            device=device,
+            dtype=dtype,
+            compile=compile,
+            trust_remote_code=trust_remote_code,
+        )
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model.eval()
-
 
         if len(self.model.config.id2label) > 1:
             label_name_fn = lambda label: f"{sanitize_model_name(model_name)}_{sanitize_model_name(label)}"
@@ -46,6 +54,28 @@ class BaseQualityClassifier:
             id_: label_name_fn(label)
             for id_, label in self.model.config.id2label.items()
         }
+
+    def _make_model(
+        self,
+        model_name: str,
+        device: str,
+        dtype: str,
+        compile: bool,
+        trust_remote_code: bool,
+    ) -> PreTrainedModel:
+        model = AutoModelForSequenceClassification.from_pretrained(
+            pretrained_model_name_or_path=model_name,
+            torch_dtype=getattr(torch, dtype),
+            trust_remote_code=trust_remote_code,
+        )
+        model = model.to(torch.device(device))
+
+        if compile:
+            model = torch.compile(model)  # pyright: ignore
+
+        model.eval()  # pyright: ignore
+
+        return model  # pyright: ignore
 
     @property
     def device(self) -> torch.device:
@@ -87,3 +117,47 @@ class Registry:
 @Registry.add("HuggingFaceFW/fineweb-edu-classifier")
 class FineWebEduClassifier(BaseQualityClassifier):
     pass
+
+
+class QualityModel(nn.Module, PyTorchModelHubMixin):
+    def __init__(self, config):
+        super(QualityModel, self).__init__()
+        self.model = AutoModel.from_pretrained(config["base_model"])
+        self.dropout = nn.Dropout(config["fc_dropout"])
+        self.fc = nn.Linear(self.model.config.hidden_size, len(config["id2label"]))
+
+    @property
+    def device(self):
+        return self.model.device
+
+    def forward(self, input_ids, attention_mask, **kwargs):
+        features = self.model(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state
+        dropped = self.dropout(features)
+        outputs = self.fc(dropped)
+        return SequenceClassifierOutput(logits=outputs[:, 0, :])
+
+
+
+@Registry.add("nvidia/quality-classifier-deberta")
+class DebertaQualityClassifier(BaseQualityClassifier):
+    def _make_model(
+        self,
+        model_name: str,
+        device: str,
+        dtype: str,
+        compile: bool,
+        trust_remote_code: bool,
+    ) -> PreTrainedModel:
+        model = QualityModel.from_pretrained(model_name)
+        model = model.to(getattr(torch, dtype))
+        model = model.to(torch.device(device))
+
+        if compile:
+            model = torch.compile(model)  # pyright: ignore
+
+        model.eval()  # pyright: ignore
+
+        # for some reason the config is not loaded automatically; need to set it manually
+        model.config = AutoConfig.from_pretrained(model_name)  # pyright: ignore
+
+        return model  # pyright: ignore
