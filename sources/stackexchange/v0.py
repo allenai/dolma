c2712739323012d8aa9b0b7d2208f01f5f868c10
Luca Soldaini <luca@soldaini.net>
2024-10-26 00:48:30 +0000

    style

--- sources/stackexchange/v0.py
+++ sources/stackexchange/v0.py
@@ -1,22 +1,91 @@
 import argparse
-import io
 import os
 import re
-import sys
 from contextlib import ExitStack
-from io import BytesIO
-from pathlib import Path
-from typing import Any, BinaryIO, Dict, Iterator, Optional
+from typing import Any, Callable, Iterator
 
-import libarchive
-import py7zr
+import libarchive  # pyright: ignore
+import py7zr  # pyright: ignore
+from resiliparse.extract.html2text import extract_plain_text  # pyright: ignore
 import pyarrow as pa
 import pyarrow.parquet as pq
-from libarchive.entry import ArchiveEntry
-from lxml import etree
+from libarchive.entry import ArchiveEntry  # pyright: ignore
+from lxml import etree  # pyright: ignore
 from tqdm import tqdm
 
 os.environ["PYTHONBREAKPOINT"] = "ipdb.set_trace"
+
+
+post_types = {
+    "1": "Question",
+    "2": "Answer",
+    "3": "Orphaned tag wiki",
+    "4": "Tag wiki excerpt",
+    "5": "Tag wiki",
+    "6": "Moderator nomination",
+    "7": "Wiki placeholder",
+    "8": "Privilege wiki",
+    "9": "Article",
+    "10": "HelpArticle",
+    "11": "Unknown",
+    "12": "Collection",
+    "13": "ModeratorQuestionnaireResponse",
+    "14": "Announcement",
+    "15": "CollectiveDiscussion",
+    "16": "CollectiveCollection",
+}
+
+POSTS_MAP: dict[str, Callable[[str | None], Any]] = {
+    "AcceptedAnswerId": lambda x: int(x or 0),
+    "AnswerCount": lambda x: int(x or 0),
+    "Body": lambda x: extract_plain_text(x or "").strip(),
+    "ClosedDate": lambda x: str(x or ""),
+    "CommentCount": lambda x: int(x or 0),
+    "CommunityOwnedDate": lambda x: str(x or ""),
+    "ContentLicense": lambda x: str(x or ""),
+    "CreationDate": lambda x: str(x or ""),
+    "Id": lambda x: int(x or 0),
+    "LastActivityDate": lambda x: str(x or ""),
+    "LastEditDate": lambda x: str(x or ""),
+    "LastEditorDisplayName": lambda x: str(x or ""),
+    "LastEditorUserId": lambda x: int(x or 0),
+    "OwnerDisplayName": lambda x: str(x or ""),
+    "OwnerUserId": lambda x: int(x or 0),
+    "ParentId": lambda x: int(x or 0),
+    "PostTypeId": lambda x: post_types.get(x or "11", "Unknown"),
+    "Score": lambda x: int(x or 0),
+    "Tags": lambda x: str(x or ""),
+    "Title": lambda x: str(x or ""),
+    "ViewCount": lambda x: int(x or 0),
+}
+
+COMMENTS_MAP: dict[str, Callable[[str | None], Any]] = {
+    "ContentLicense": lambda x: str(x or ""),
+    "CreationDate": lambda x: str(x or ""),
+    "Id": lambda x: int(x or 0),
+    "PostId": lambda x: int(x or 0),
+    "Score": lambda x: int(x or 0),
+    "Text": lambda x: str(x or ""),
+    "UserDisplayName": lambda x: str(x or ""),
+    "UserId": lambda x: int(x or 0),
+}
+
+USERS_MAP: dict[str, Callable[[str | None], Any]] = {
+    "Id": lambda x: int(x or 0),
+    "Reputation": lambda x: int(x or 0),
+    "CreationDate": lambda x: str(x or ""),
+    "DisplayName": lambda x: str(x or ""),
+    "LastAccessDate": lambda x: str(x or ""),
+    "WebsiteUrl": lambda x: str(x or ""),
+    "Location": lambda x: str(x or ""),
+    "AboutMe": lambda x: str(x or ""),
+    "Views": lambda x: int(x or 0),
+    "UpVotes": lambda x: int(x or 0),
+    "DownVotes": lambda x: int(x or 0),
+    "ProfileImageUrl": lambda x: str(x or ""),
+    "EmailHash": lambda x: str(x or ""),
+    "AccountId": lambda x: int(x or 0),
+}
 
 
 def get_7z_uncompressed_size(sz_path, entry_name):
@@ -67,20 +136,25 @@ def stream_xml_from_7z(
             for chunk in entry.get_blocks(block_size):
                 pbar.update(len(chunk))
                 first_seg, *segments = re.split(b"\r*\n|\r", chunk)
-                yield prev_line + first_seg
-                yield from segments[:-1]
-                prev_line = segments[-1]
+                if segments:
+                    # there's at least one line break in the chunk, so we can yield the previous line
+                    yield prev_line + first_seg
+                    yield from segments[:-1]
+                    prev_line = segments[-1]
+                else:
+                    # no line breaks in the chunk, so we need to accumulate it
+                    prev_line += chunk
 
 
 def process_file(
     archive_path: str,
     output_dir: str,
     entry_name: str,
+    entry_map: dict[str, Callable[[str| None], Any]],
     batch_size: int = 100_000,
     block_size: int = 8192,
 ):
     entry_prefix, _ = os.path.basename(entry_name.lower()).split(".", 1)
-    output_dir = os.path.join(output_dir, entry_prefix)
     archive_name = os.path.basename(archive_path)
 
     os.makedirs(output_dir, exist_ok=True)
@@ -101,7 +175,7 @@ def process_file(
             if not row.attrib:
                 continue
 
-            data.append(dict(row.attrib))
+            data.append({k: v(row.attrib.get(k, None)) for k, v in entry_map.items()})
 
             if schema is None:
                 schema = pa.Table.from_pylist(data).schema
@@ -140,19 +214,19 @@ def main():
         archive_paths = [
             os.path.join(args.archive_path, p) for p in os.listdir(args.archive_path) if p.endswith("7z")
         ]
-        output_paths = [os.path.join(args.output_dir, os.path.basename(p)) for p in archive_paths]
     else:
         archive_paths = [args.archive_path]
-        output_paths = [args.output_dir]
 
-    for archive_path, output_path in tqdm(
-        zip(archive_paths, output_paths), desc="Archives", total=len(archive_paths)
-    ):
-        for entry_name in ["Posts.xml", "Comments.xml"]:
+    for archive_path in tqdm(archive_paths, desc="Archives"):
+        for entry_name, entry_map in [("Posts.xml", POSTS_MAP), ("Comments.xml", COMMENTS_MAP), ("Users.xml", USERS_MAP)]:
+            clean_entry_name = entry_name.split(".", 1)[0].lower()
+            clean_forum_name = archive_path.split("/")[-1].rsplit(".", 1)[0].lower().replace(".", "_")
+            output_path = os.path.join(args.output_dir, clean_entry_name, f"forum={clean_forum_name}")
             process_file(
                 archive_path=archive_path,
                 output_dir=output_path,
                 entry_name=entry_name,
+                entry_map=entry_map,  # pyright: ignore
                 batch_size=args.batch_size,
                 block_size=args.block_size,
             )
