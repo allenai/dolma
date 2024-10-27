e1137dc00ccc24eb6034d50a0c742b3509530498
David Graham <david.l.graham1@gmail.com>
2024-10-27 16:19:40 -0700

    temp - always use rank 0

--- classifiers/src/dolma_classifiers/utils.py
+++ classifiers/src/dolma_classifiers/utils.py
@@ -22,10 +22,10 @@ def get_rank_and_world_size():
 
 def get_local_gpu_rank() -> int:
     """Returns the local GPU rank for the current process using torch.distributed."""
-    if dist.is_initialized():
-        return dist.get_rank() % torch.cuda.device_count()
-    else:
-        return 0
+    #if dist.is_initialized():
+   #     return dist.get_rank() % torch.cuda.device_count()
+   # else:
+    return 0
 
 
 def setup() -> tuple[int, int]:
