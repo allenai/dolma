3c32df11b75d897ac43f188f389ac4c349519a60
David Graham <david.l.graham1@gmail.com>
2024-11-04 13:52:49 -0800

    -

--- classifiers/src/dolma_classifiers/inference.py
+++ classifiers/src/dolma_classifiers/inference.py
@@ -429,7 +429,6 @@ def main(args: argparse.Namespace) -> None:
     console_logger.info(f"Partitioned into {world_size} workers of with avg {files_per_process:.2f} files.")
     console_logger.info(f"GPU {rank}/{world_size} processing {len(partition_source_paths)} files from index {start_idx} to {end_idx}")
     os.environ["CUDA_VISIBLE_DEVICES"] = str(rank)
-    export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
     chunk_size = 8
     n_chunks = math.ceil(len(partition_source_paths) / chunk_size)
     actual_chunk_size = math.ceil(len(partition_source_paths) / n_chunks)
