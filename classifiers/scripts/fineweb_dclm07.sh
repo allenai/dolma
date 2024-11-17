2afe2678314e1a7f9ef3d2422971794dfb4128d9
David Graham <david.l.graham1@gmail.com>
2024-11-16 21:25:14 -0800

    -

--- classifiers/scripts/fineweb_dclm07.sh
+++ classifiers/scripts/fineweb_dclm07.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-DOCUMENTS='s3://ai2-llm/pretraining-data/sources/dclm/v0_rep32_ft7percentile/documents/*zst'
+DOCUMENTS='s3://ai2-llm/pretraining-data/sources/dclm/refinedweb/documents/global-shard_01_of_10/local-shard_0_of_10/*zstd'
 
 NUM_NODES=4
 MODEL_NAME="HuggingFaceFW/fineweb-edu-classifier"
-CLUSTER="ai2/jupiter*"
+CLUSTER="ai2/augusta-google-*"
 BATCH_SIZE=1024
-PRIORITY="urgent"
+PRIORITY="high"
 
 # Generate a hash for the run name by combining model name and documents
 RUN_HASH=$(echo -n "${MODEL_NAME}${DOCUMENTS}" | md5sum | awk '{print $1}')
@@ -20,7 +20,7 @@ gantry run \
     --task-name "${RUN_NAME}" \
     --description "Score ${DOCUMENTS} with ${MODEL_NAME}" \
     --allow-dirty \
-    --workspace ai2/davidw-oe-annealing \
+    --workspace ai2/oe-data \
     --beaker-image 'petew/olmo-torch23-gantry' \
     --timeout -1 \
     --show-logs \
@@ -36,9 +36,9 @@ gantry run \
     --env LOG_FILTER_TYPE=local_rank0_only \
     --env OMP_NUM_THREADS=8 \
     --env BEAKER_USER_ID=$(beaker account whoami --format json | jq '.[0].name' -cr) \
-    --env-secret AWS_ACCESS_KEY_ID=lucas-AWS_ACCESS_KEY_ID \
-    --env-secret AWS_SECRET_ACCESS_KEY=lucas-AWS_SECRET_ACCESS_KEY \
-    --env-secret WANDB_API_KEY=lucas-WANDB_API_KEY \
+    --env-secret AWS_ACCESS_KEY_ID=davidg_AWS_ACCESS_KEY_ID \
+    --env-secret AWS_SECRET_ACCESS_KEY=davidg_AWS_SECRET_ACCESS_KEY \
+    --env-secret WANDB_API_KEY=davidg_WANDB_API_KEY \
     --shared-memory 10GiB \
     --install "pip install -e classifiers/" \
     --yes \
