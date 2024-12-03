4dc0d5293aeef794deceaec7b0d84bf0c066b0ab
Ben Bogin <benb@allenai.org>
2024-12-02 16:01:34 -0800

    .

--- classifiers/src/dolma_classifiers/inference.py
+++ classifiers/src/dolma_classifiers/inference.py
@@ -174,7 +174,7 @@ def writer_worker(
                 counts[source] += len(attributes)
                 total_count += len(attributes)
 
-                console_logger.info(f"Writing {len(attributes)} documents to {source_destination_mapping[source]}, total {counts[source]}, stop at {stop_at}. stop at is not None: {stop_at is not None}, total count >= stop_at: {total_count >= stop_at}")
+                console_logger.info(f"Writing {len(attributes)} documents to {source_destination_mapping[source]}, total {counts[source]}, stop at {stop_at}. total_count {total_count}. stop at is not None: {stop_at is not None}, total count >= stop_at: {total_count >= stop_at}")
 
                 if stop_at is not None and total_count >= stop_at:
                     console_logger.info(f"Reached stop_at limit of {stop_at} documents")
