ed6d834f51c6e10c41971cc39cfa06461229f58b
Chris Wilhelm <chris.wilhelm@gmail.com>
2025-02-12 16:48:44 -0800

    Implements initial pass of learn2code heuristic filters (#230)
    This is a combination of @revbucket 's desired heuristic filters for learn2code,
    plus the existing filters we had previously implemented in prior iterations.
    
    A starting point. I am a bit suspicious about the precision of the regexes
    I lifted from the StarCoder paper.
    
    Note: plan is to merge this into long-lived "learn2code"
    feature branch rather than main, as I suspect we will be iterating
    a lot.

--- python/dolma/taggers/code/__init__.py
+++ python/dolma/taggers/code/__init__.py
@@ -4,6 +4,7 @@ from .code_taggers import (
     CodeSecretsTagger,
     CodeStarCoderTaggers,
     CodeStarCoderTaggers2,
+    Learn2CodeTaggers,
 )
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "CodeRedPajamaTaggers",
     "CodeStarCoderTaggers",
     "CodeStarCoderTaggers2",
+    "Learn2CodeTaggers",
 ]
