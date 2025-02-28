e71559103501d6c9484762cd120113b4b21c83e6
robert berry <berrydigital@gmail.com>
2025-02-28 14:01:19 -0800

    Add code-prose tagger to init import.
    Additionally update commentary and word choice.

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -4,11 +4,11 @@ import pytest
 
 
 # The default multiprocessing start method is "fork" which is not compatible with
-# with runtime assertions that it is set to spawn. When running unit tests, it's
-# possible to call an external library that sets the start method to "fork".
-# Here we enforce the start method to be "spawn" for all tests before executing.
+# with runtime assertions that it is set to "spawn". When running unit tests, it's
+# possible to call an external library that sets the start method to the default.
+# Here we set the start method to be "spawn" for all tests before executing.
 @pytest.fixture(scope="session", autouse=True)
-def initialize_data_environment():
+def initialize_multiprocessing_start_method():
     try:
         multiprocessing.set_start_method("spawn")
     except Exception:
