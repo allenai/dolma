6f8d44db594d7a586b3029cabbb986ddf72bec6a
Allyson Ettinger <allysone@allenai.org>
2025-06-26 10:57:07 -0400

    add lowthreshonly

--- python/dolma/taggers/reddit.py
+++ python/dolma/taggers/reddit.py
@@ -166,6 +166,15 @@ class DenseMMLUSubsHighThresh(ListMembership):
         score = doc.metadata["subreddit"].lower() in self.blocklist
         return DocResult(doc=doc, spans=[Span(start=0, end=len(doc.text), type="doc", score=score)])
     
+
+@TaggerRegistry.add("dense_mmlu_subreddit_lowthresh")
+class DenseMMLUSubsLowThresh(ListMembership):
+    LOOKUP_LIST = (Path(__file__).parent / "../data/reddit_blocklists/dense_mmlu_topic_subreddits_lowthreshonly.txt")
+
+    def predict(self, doc: Document) -> DocResult:
+        score = doc.metadata["subreddit"].lower() in self.blocklist
+        return DocResult(doc=doc, spans=[Span(start=0, end=len(doc.text), type="doc", score=score)])
+    
 @TaggerRegistry.add("science_subreddit")
 class ScienceSubs(ListMembership):
     LOOKUP_LIST = (Path(__file__).parent / "../data/reddit_blocklists/sciencesubreddits.txt")
